a225cb0b6f203443cc603b1ba0449daa617721e3
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-01-04 20:06:21 -0300

    Add new Class UserMap

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -1,3 +1,4 @@
+using Api.Data.Mapping;
 using Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,7 @@ namespace Api.Data.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
         }
     }
 }
